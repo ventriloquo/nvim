f2c8bfdc47322170352ac2aa407fcac628fcb7df
ventriloquo <dfern3321@gmail.com>
2025-05-13 02:04:54 -0300

    Pequeno ajuste

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -3,7 +3,11 @@ return {
     "nvim-tree/nvim-tree.lua",
     dependencies = "nvim-tree/nvim-web-devicons",
     config = function ()
-      require("nvim-tree").setup()
+      require("nvim-tree").setup({
+        view = {
+          width = 25,
+        }
+      })
     end
   }
 }

b91f6b9c30835a117c06d62888641a1e67374286
ventriloquo <dfern3321@gmail.com>
2025-05-13 02:05:07 -0300

    Painel de símbolos

---
+++ lua/plugins/symbols.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    "oskarrrrrrr/symbols.nvim",
+    config = function()
+        local r = require("symbols.recipes")
+        require("symbols").setup(
+            r.DefaultFilters, r.AsciiSymbols
+        )
+        vim.keymap.set("n", ",s", "<cmd>Symbols<CR>")
+        vim.keymap.set("n", ",S", "<cmd>SymbolsClose<CR>")
+    end
+  }
+}
